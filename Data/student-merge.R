cacbacbf7bbddd4b8b6a6d03fec7f85b17869279
Matt Wasyluk <mwasylu@ncsu.edu>
2024-10-11 23:48:57 -0400

    submit

---
+++ Data/student-merge.R
@@ -0,0 +1,5 @@
+d1=read.table("Data/student-mat.csv",sep=";",header=TRUE)
+d2=read.table("Data/student-por.csv",sep=";",header=TRUE)
+
+d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
+print(nrow(d3)) # 382 students
